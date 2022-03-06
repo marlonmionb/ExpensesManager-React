4828c75b134785ff1f264ea41651386e52d351c7
marlon bohn <marlonmionb@gmail.com>
2022-03-06 19:05:20 -0300

    final version

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -22,6 +23,7 @@ function Expenses(props) {
       selected={filteredYear}
       onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList items={filteredExpenses} />
     </Card>
   )
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,9 +23,9 @@ function ExpenseForm(props) {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        };
+            amount: +enteredAmount,
+            date: new Date(enteredDate),
+          };
 
         props.onSaveExpenseData(expenseData);
 
